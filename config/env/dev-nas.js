779ae52b79702ae3f03904366e1a0f2a906cebc3
Kostas Siabanis <kostas.siabanis@gmail.com>
2015-03-04 22:43:42 +0200

    Added environment file for local NAS

--- config/env/dev-nas.js
+++ config/env/dev-nas.js
@@ -1,23 +1,10 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/groovity-starter',
-	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-			],
-			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
-			]
-		},
-		css: 'public/dist/application.min.css',
-		js: 'public/dist/application.min.js'
+    walkPath: '/volume1/RaiNAS/Music',
+	db: 'mongodb://192.168.1.105/groovity-starter-nas',
+	app: {
+        title: 'The Groovity starter'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
