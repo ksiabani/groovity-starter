7627942e9b78cd86451d25fcbf3046af97ab6e0d
Kostas Siabanis <kostas.siabanis@gmail.com>
2015-03-04 16:59:08 +0200

    Enabled CORS

--- app/routes/tracks.server.routes.js
+++ app/routes/tracks.server.routes.js
@@ -4,6 +4,13 @@ module.exports = function(app) {
 	var users = require('../../app/controllers/users.server.controller');
 	var tracks = require('../../app/controllers/tracks.server.controller');
 
+    //Enable CORS
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
+
 	// Tracks Routes
 	app.route('/tracks')
 		.get(tracks.list)
