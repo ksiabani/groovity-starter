9d1d991c10a0e765a32acd1d9530440435b5f52b
Kostas Siabanis <kostas.siabanis@gmail.com>
2015-03-23 12:14:16 +0200

    Working with ffmpeg

--- config/copier.js
+++ config/copier.js
@@ -7,17 +7,18 @@ var fs = require('fs'),
     readdirp = require('readdirp'),
     config = require('../config/config'),
     ID3 = require('id3v2-parser'),
-    mm= require('musicmetadata'),
+    mm = require('musicmetadata'),
     mongoose = require('mongoose'),
     Track = mongoose.model('Track'),
     logger = require('../config/logger'),
     md5 = require('MD5'),
     async = require('async'),
     S = require('string'),
-    fsSync = require('fs-sync'),
+//fsSync = require('fs-sync'),
     sanitize = require('sanitize-filename'),
     child_process = require('child_process'),
-    execFile = require('child_process').execFile;
+    execFile = require('child_process').execFile,
+    spawn = require('child_process').spawn;
 
 
 //var spawn = require('child_process').spawn,
@@ -43,16 +44,19 @@ module.exports = function () {
     async.waterfall([
 
         function (callback) {
-            Track.find({copied: false}, {
-                source: 1,
-                album: 1,
-                artist: 1,
-                title: 1,
-                cover: 1
-            }).exec(function (err, tracks) {
-                if (err) logger.error(err);
-                callback(null, tracks);
-            });
+            Track
+                .find({copied: false}, {
+                    source: 1,
+                    album: 1,
+                    artist: 1,
+                    title: 1,
+                    cover: 1
+                })
+                .limit(1)
+                .exec(function (err, tracks) {
+                    if (err) logger.error(err);
+                    callback(null, tracks);
+                });
         },
 
         function (tracks, callback) {
@@ -63,7 +67,28 @@ module.exports = function () {
                 //var source = track.source;
                 //var target = config.destPath + sanitize(track.album.replace(/ /g, '_') + '-' + track.artist.replace(/ /g, '_') + '-' + track.title.replace(/ /g, '_')) + '.mp3';
                 //var ffmpeg = execFile('ffmpeg', ['-y', '-i', track.source, config.artPath + '/' + track.cover]);
-                var ffmpeg = execFile('ffmpeg', ['-y', '-i', track.source, '-c:a', 'copy', config.artPath + '/' + 'out.mp3']);
+                //var ffmpeg = execFile('ffmpeg', ['-y', '-i', track.source, '-c:a', 'copy', config.artPath + '/' + 'out.mp3']);
+                //windows
+                var ffmpeg = spawn(process.env.comspec, ['/c',
+                    'C:\\Temp\\ffmpeg\\bin\\ffmpeg',                        //command
+                    '-y',                                                   //global switches
+                    '-i', track.source,                                     //input
+                    '-c:a', 'copy', config.artPath + '/' + 'out1.mp3',      //first output, just copy the file
+                    '-metadata', 'track= ', 'encoded_by= ', 'Supplier= ', 'Ripping tool= ', 'encoder= ', //set metadata 'Catalog \#= ', 'Rip date= ', 'Release type= ', 'Source= ',
+                    '-b:a', '128k', '-f', 'mp3', config.artPath + '/' + 'out2.mp3', //second output, converting to 128
+                    '-f', 'ffmetadata', config.artPath + '/' +'metadata.txt',       //extract metadata to a file
+                    config.artPath + '/' + track.cover                              //extract artwork
+                ]);
+                //ffmpeg -i son_origine.avi -vn -ar 44100 -ac 2 -ab 192 -f mp3 son_final.mp3
+                //ffmpeg -i original.mp4 -metadata title="my title" -c copy -y /tmp/tmp.mp4 && ffmpeg -i /tmp/tmp.mp4 -c copy -y original.mp4
+
+                //cp.stdout.on("data", function(data) {
+                //    console.log(data.toString());
+                //});
+                //
+                //cp.stderr.on("data", function(data) {
+                //    console.error(data.toString());
+                //});
 
                 ffmpeg.stdout.on('data', function (data) {
                     console.log('stdout: ' + data);
